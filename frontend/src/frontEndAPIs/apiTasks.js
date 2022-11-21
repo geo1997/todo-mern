95c85e2bad6325236de2ba5dc45cec05ab6a7361
IT18148428_K.G.G.H.Silva <kgeorgesilva97@gmail.com>
2022-11-21 13:27:07 +0530

    Front end apis

--- frontend/src/frontEndAPIs/apiTasks.js
+++ frontend/src/frontEndAPIs/apiTasks.js
@@ -31,3 +31,51 @@ export const createTask  = (task) => {
             console.log(error);
         })
 }
+
+export const updateTaskData = (id,task) =>{
+    return fetch(`${API}/update/${id}`,{
+        method: "PATCH",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            // Authorization: `Bearer ${token}`
+        },
+        body: JSON.stringify(task)
+    })
+        .then(response =>{
+            return response.json();
+        })
+        .catch(err=> console.log(err));
+};
+
+export const getTaskData = (id) =>{
+    return fetch(`${API}/user/${id}`,{
+        method: "GET",
+        headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            //Authorization: `Bearer ${token}`
+        }
+    })
+        .then(response =>{
+            return response.json();
+        })
+        .catch(err=> console.log(err));
+};
+
+
+export const deleteTask = (taskId ) =>{
+    return fetch(`${API}/delete/${taskId}`,{
+        method: "DELETE",
+        headers :{
+            Accept: "application/json",
+            "Content-Type":"application/json",
+            //Authorization: `Bearer ${token}`
+        },
+    })
+        .then(response =>{
+            return response.json();
+        })
+        .catch(err => console.log(err));
+
+};
---
+++ frontend/src/frontEndAPIs/main.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+const url = 'http://localhost:8000/tasks '
+
+export const retrieveTasks = () =>axios.get('url');
